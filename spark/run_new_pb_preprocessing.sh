acfb9b8914ff0a860dec9cf362aa683814567d48
주언 <137984851+sulsikan@users.noreply.github.com>
2025-07-23 14:45:26 +0900

    feat: 새로 업데이트 되는 PB 데이터 전처리 자동화 쉘스크립트 추가

---
+++ spark/run_new_pb_preprocessing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BRANDS=("biohealboh" "bringgreen" "roundaround" "idealforman" "careplus" "delightproject")
+BUCKET="de6-team5-bucket"
+TODAY=$(date +%Y%m%d)
+
+for BRAND in "${BRANDS[@]}"
+do
+    INPUT_PREFIX="s3://${BUCKET}/raw_data/pb/${BRAND}/"
+    S3A_INPUT_PREFIX="s3a://${BUCKET}/raw_data/pb/${BRAND}/"
+    S3A_OUTPUT_PREFIX="s3a://${BUCKET}/preprocessed_data/pb/${BRAND}/"
+    FILES=$(aws s3 ls "${INPUT_PREFIX}" | awk '{print $4}' | grep "${TODAY}")
+
+    for file in $FILES
+    do
+        spark-submit /opt/bitnami/spark/jobs/data_preprocessing.py \
+            "${S3A_INPUT_PREFIX}${file}" "${S3A_OUTPUT_PREFIX}"
+    done
+done
